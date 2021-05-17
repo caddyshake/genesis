67c7a23ed5aae2ddaa2063cfbacc6942c104c580
ank_hmi <ank@designlogic.us>
2021-05-17 19:55:45 +0300

    Task 1.2 Terraform

--- 1.2/terraform/launch_template.tf
+++ 1.2/terraform/launch_template.tf
@@ -3,43 +3,12 @@ provider "aws" {
     region = "us-west-2"
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
-
-  tags = {
-    Name = "genesis"
-  }
-}
-
-resource "aws_subnet" "genesis-subnet " {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "genesis"
-  }
-}
-
-resource "aws_network_interface" "genesis" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
-
-resource "aws_instance" "foo" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+resource "aws_instance" "instance_genesis" {
+  ami           = "ami-03d5c68bab01f3496" # us-west-2
   instance_type = "t2.micro"
-
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
+  key_name = "mx-linux"
+  tags = {
+    Name = "genesis"
   }
 
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
 }
